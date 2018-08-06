9924fa1d7f35212da92a7a1147e1ecdd19633354
Albert-91 <albert.suralinski@gmail.com>
2018-08-06 07:36:42 +0200

    Exercise 6. Added ajax method but it doesn't work

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(function(){
     .done(function(response){
         var $titles_list = $('#titles');
         for(var i = 0; i < response.length; i++){
-            var $title = $('<li>' + response[i].title + '	<button id="' + response[i].id + '">Delete</button></li>')
+            var $title = $('<li>' + response[i].title + '	</li><button class="delete">Delete</button>')
             var author = "<li>Author: " + response[i].author + "</li>"
             var isbn = "<li>ISBN: " + response[i].isbn + "</li>"
             var publisher = "<li>Publisher: " + response[i].publisher + "</li>"
@@ -24,7 +24,7 @@ $(function(){
             $titles_list.append($title);
             $titles_list.append($new_div);
         }
-        var $titles = $titles_list.children()
+        var $titles = $titles_list.first()
         $titles.on('click', function(){ 
             $(this).next('div').toggle('display') 
         })
@@ -51,5 +51,17 @@ $(function(){
         })
 
     })
-    
+    var $delete = $('.delete')
+	$delete.on('click', function(event){
+		event.preventDefault()
+		$.ajax({
+            url: "http://127.0.0.1:8000/book/",
+            method: "DELETE",
+            data: $(this).prev(),
+            dataType: "json"
+        })
+        .done(function(response){
+            location.reload()
+        })
+	})
 });
